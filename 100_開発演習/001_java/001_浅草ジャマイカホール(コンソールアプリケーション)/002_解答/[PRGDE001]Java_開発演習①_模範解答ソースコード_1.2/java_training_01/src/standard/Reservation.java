9847d68b9ff41e93fa5e4a8bb775c57c8fd55b87
秋山（札幌2B） <create0stance@gmail.com>
2023-04-21 18:19:27 +0900

    add

--- 100_開発演習/001_java/001_浅草ジャマイカホール(コンソールアプリケーション)/002_解答/[PRGDE001]Java_開発演習①_模範解答ソースコード_1.2/java_training_01/src/standard/Reservation.java
+++ 100_開発演習/001_java/001_浅草ジャマイカホール(コンソールアプリケーション)/002_解答/[PRGDE001]Java_開発演習①_模範解答ソースコード_1.2/java_training_01/src/standard/Reservation.java
@@ -17,10 +17,8 @@ public class Reservation {
      */
     public static void main(String[] args) throws IOException {
 
-        // 座席情報(30席)trueの場合は予約済み
         boolean[] seatArray = new boolean[30];
 
-        // 実行確認用に15番と21番を予約済みにしておく
         seatArray[14] = true;
         seatArray[20] = true;
 
@@ -28,25 +26,18 @@ public class Reservation {
         System.out.println("何番の座席を予約しますか？（1～30）");
         System.out.print("＞");
 
-        // 入力準備
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
-        // 座席番号の入力処理
         String seatNumStr = br.readLine();
 
-        // 座席番号を数値に変換
         int seatNum = Integer.parseInt(seatNumStr);
 
-        // 入力値が0～29ではなく1～30なので-1で補正する
         seatNum--;
 
-        // 指定された座席が予約済みであった場合
         if (seatArray[seatNum]) {
             System.out.println("\n※予約済みの座席です。他の座席を選択してください。");
 
-            // 指定された座席がまだ予約されていなかった場合
         } else {
-            // 指定された座席を予約済みにする
             seatArray[seatNum] = true;
             System.out.println("\n座席の予約が完了しました。");
         }
