a03482dd4366a27e8a3354e6f06bb1f3e1da615c
crearestance <management@create-stance.com>
2023-04-24 06:48:01 +0900

    [java2]

---
+++ 01_講義/005_JAVA/ADD＿/022_プログラミング演習/008_プログラミング演習_基本構文/001_問題/[PRG001]Java_excercise_basic_syntax_question/Java_excercise_basic_syntax_question/src/section03_repetition/challenge01/PrimeNumber_level1.java
@@ -0,0 +1,38 @@
+/**
+ * 素数判定
+ * 特定の数字１個の判定
+ *
+ * 1と自分自身以外に正の約数を持たない自然数のことを素数と言います。
+ * 言い換えると、2以上n未満の約数を持たない自然数nを素数といいます。
+ * ※ちなみに、自然数nを割り切れる自然数xはn/2以下の数字です。
+ *
+ * 入力した整数が素数であることを判定するプログラムを作成してください。
+ *
+ * 実行例：
+ * （素数である場合）
+ * 整数を入力してください＞2749
+ * 2749は素数です
+ *
+ * （素数でない場合）
+ * 整数を入力してください＞100
+ * 100は素数ではありません。
+ *
+ * ====
+ * ScannerクラスはBufferedReaderクラスの仲間で、標準入力するための機能を持ちます。
+ * 文字列を入力する場合は「String target = stdIn.next();」のように使います。
+ * 整数を入力する場合は「int target = stdIn.nextInt();」のように使います。
+ * BRと違って、文字と数値の変換が不要です。
+ */
+
+package section03_repetition.challenge01;
+
+import java.util.Scanner;
+
+public class PrimeNumber_level1 {
+
+	public static void main(String[] args) {
+		Scanner stdIn = new Scanner(System.in);
+
+	}
+
+}
---
+++ 01_講義/005_JAVA/ADD＿/022_プログラミング演習/008_プログラミング演習_基本構文/001_問題/[PRG001]Java_excercise_basic_syntax_question/Java_excercise_basic_syntax_question/src/section03_repetition/challenge02/PrimeNumber_level2.java
@@ -0,0 +1,30 @@
+/**
+ * 素数判定
+ * 範囲内の数字の素数判定
+ * 
+ * 1と自分自身以外に正の約数を持たない自然数のことを素数と言います。
+ * 言い換えると、2以上n未満の約数を持たない自然数nを素数といいます。
+ * ※ちなみに、自然数nを割り切れる自然数xはn/2以下の数字です。
+ * 
+ * 2～入力した整数までの数字の中で、素数である数字を抽出する
+ * プログラムを作成してください。
+ * 
+ * 実行例：
+ * 整数を入力してください＞20
+ * 20までの素数は以下の数字です。
+ * 2, 3, 5, 7, 11, 13, 17, 19, 
+ * 
+ */
+
+package section03_repetition.challenge02;
+
+import java.util.Scanner;
+
+public class PrimeNumber_level2 {
+
+	public static void main(String[] args) {
+		Scanner stdIn = new Scanner(System.in);
+		
+	}
+
+}
